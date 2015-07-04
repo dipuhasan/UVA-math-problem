701e7e991c99808466dbc852da3168d7e7865bf8
dipuhasan <depu1994@gmail.com>
2015-07-04 10:01:40 +0600

    11296 - Counting Solutions to an Integral Equation.cpp

---
+++ 11296 - Counting Solutions to an Integral Equation.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long n;
+    while(scanf("%lld", &n)==1){
+        printf("%lld\n", (n/2+2) * (n/2+1)/2);
+    }
+
+    return 0;
+}
